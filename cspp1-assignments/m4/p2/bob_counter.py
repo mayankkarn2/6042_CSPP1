494b70e17d9d36c393fe5016c5cec3ffd0b3878a
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-02 22:21:22 +0530

    " -> 5 of 5 passed. pylint: 9.57/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -1,34 +1,35 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the number of times the string 'bob' occurs in s. 
-For example, if s = 'azcbobobegghakl', 
+Write a program that prints the number of times the string 'bob' occurs in s.
+For example, if s = 'azcbobobegghakl',
 then your program should print
 Number of times bob occurs is: 2'''
 
 def main():
-    '''This programs counts the nimber of 
+    '''This programs counts the number of
     instances of the string 'Bob' in the given string'''
-    STR_INPUT = input()
-    CHECK_STRING = 'bob'
-    STR_LEN = len(STR_INPUT)
-    START_VALUE = 0
-    END_VALUE = 2
-    COUNT_VALUE = 0
-    while END_VALUE <= (STR_LEN)-1:
-        COUNTER_I = START_VALUE
-        EQUALS_FLAG = 1
-        COUNTER_J = 0
-        while COUNTER_I <= END_VALUE and EQUALS_FLAG == 1:
-            if STR_INPUT[COUNTER_I] != CHECK_STRING[COUNTER_J]:
-                EQUALS_FLAG = 0
+    str_input = input()
+    check_string = 'bob'
+    str_len = len(str_input)
+    start_value = 0
+    end_value = 2
+    count_value = 0
+    while end_value <= (str_len)-1:
+        counter_i = start_value
+        equals_flag = 1
+        counter_j = 0
+        while counter_i <= end_value and equals_flag == 1:
+            if str_input[counter_i] != check_string[counter_j]:
+                equals_flag = 0
             else:
-                COUNTER_I = COUNTER_I + 1
-                COUNTER_J = COUNTER_J + 1
-        if EQUALS_FLAG == 1:
-                COUNT_VALUE = COUNT_VALUE + 1
-        START_VALUE = START_VALUE + 1
-        END_VALUE = END_VALUE + 1
-    print(COUNT_VALUE)
+                counter_i = counter_i + 1
+                counter_j = counter_j + 1
+        if equals_flag == 1:
+                count_value = count_value + 1
+        start_value = start_value + 1
+        end_value = end_value + 1
+    print(count_value)
+
 
 if __name__ == "__main__":
     main()
