92c17bec42003e9e2d9431c81d1e08da300007db
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-02 22:08:32 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -1,14 +1,34 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
-
+Write a program that prints the number of times the string 'bob' occurs in s. 
+For example, if s = 'azcbobobegghakl', 
+then your program should print
 Number of times bob occurs is: 2'''
 
 def main():
-	s = raw_input()
-	# the input string is in s
-	# remove pass and start your code here
-	pass
+   '''This programs counts the nimber of 
+      instances of the string 'Bob' in the given string'''
+    STR_INPUT = input()
+    CHECK_STRING = 'bob'
+    STR_LEN = len(STR_INPUT)
+    START_VALUE = 0
+    END_VALUE = 2
+    COUNT_VALUE = 0
+    while END_VALUE <= (STR_LEN)-1:
+        COUNTER_I = START_VALUE
+        EQUALS_FLAG = 1
+        COUNTER_J = 0
+        while COUNTER_I <= END_VALUE and EQUALS_FLAG == 1:
+            if STR_INPUT[COUNTER_I] != CHECK_STRING[COUNTER_J]:
+                EQUALS_FLAG = 0
+            else:
+                COUNTER_I = COUNTER_I + 1
+                COUNTER_J = COUNTER_J + 1
+            if EQUALS_FLAG == 1:
+        COUNT_VALUE = COUNT_VALUE + 1
+        START_VALUE = START_VALUE + 1
+        END_VALUE = END_VALUE + 1
+print(COUNT_VALUE)
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
