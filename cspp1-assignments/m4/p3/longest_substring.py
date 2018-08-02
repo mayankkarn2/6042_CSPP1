c0795c9e704f55ac6d8dfb43c66199ca2a60f200
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-02 22:43:46 +0530

    " -> 5 of 5 passed. pylint: 9.31/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -16,33 +16,34 @@ If you have time, come back to this problem after you've had a break and cleared
 
 def main():
     """This program prints the longest sequence of alphabets"""
-    STRING_INPUT = input()
-    STRING_A = STRING_INPUT + "!"
-    TEMP = ''
-    TEMP_1 = ''
-    BEG_VAL = 0
-    MOV_VAL = 1
-    LEN = len(STRING_A)
-    COUNT = 1
-    LENGTH = 1
-    while MOV_VAL <= LEN-1:
-        COUNT = 1
-        TEMP = STRING_A[BEG_VAL]
-        while STRING_A[BEG_VAL] <= STRING_A[MOV_VAL] and MOV_VAL < LEN:
-            COUNT = COUNT + 1
-            TEMP = TEMP+STRING_A[MOV_VAL]
-            BEG_VAL = MOV_VAL
-            MOV_VAL = MOV_VAL + 1
-        BEG_VAL = MOV_VAL
-        MOV_VAL = MOV_VAL + 1
-        if COUNT == LENGTH:
-            TEMP_1 = TEMP_1
+    string_input = input()
+    string_a = string_input + "!"
+    temp = ''
+    temp_1 = ''
+    beg_val = 0
+    mov_val = 1
+    leng = len(string_a)
+    count = 1
+    length = 1
+    while mov_val <= leng-1:
+        count = 1
+        temp = string_a[beg_val]
+        while string_a[beg_val] <= string_a[mov_val] and mov_val < leng:
+            count = count + 1
+            temp = temp+string_a[mov_val]
+            beg_val = mov_val
+            mov_val = mov_val + 1
+        beg_val = mov_val
+        mov_val = mov_val + 1
+        if count == length:
+            temp_1 = temp_1
 
-        if COUNT > LENGTH:
-            LENGTH = COUNT
-            TEMP_1 = ""
-            TEMP_1 = TEMP
-    print(TEMP_1)
+        if count > length:
+            length = count
+            temp_1 = ""
+            temp_1 = temp
+    print(temp_1)
+
 
 
 if __name__ == "__main__":
