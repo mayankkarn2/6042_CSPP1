1dcbfe2854b1a41de602aebd9ce8120d5ac718bf
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-03 21:30:25 +0530

    " -> 3 of 3 passed. pylint: 4.55/10 "

--- cspp1-assignments/m5/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p4/square_root_newtonrapson.py
@@ -10,15 +10,14 @@
 # output: 6.999999999999991
 
 def main():
-	input_n = int(input())
-	guess_n=input_n/2
-	diff=0.01
-	while 1:
-		if abs(guess_n*guess_n-input_n)>=diff:
-			guess_n=((guess_n) - ((guess_n*guess_n-input_n)/(2 *(guess_n))))
-		else:
-			break;
-	print(guess_n)
-
+    input_n = int(input())
+    guess_n=input_n/2
+    diff=0.01
+    while 1:
+        if abs(guess_n*guess_n-input_n)>=diff:
+            guess_n = ((guess_n) - ((guess_n*guess_n-input_n)/(2 *(guess_n))))
+        else:
+            break;
+    print(guess_n)
 if __name__ == "__main__":
-	main()
+    main()
