6328c54ea1bc7e46c1b3f7693e9dc0783b82a351
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-03 21:19:23 +0530

    " -> 3 of 3 passed. pylint: 5.83/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -10,18 +10,18 @@
 # output: 6.999999999999991
 
 def main():
-	
-	epsilon = 0.01
-	step = 0.1
-	num = int(input())
-	guess = 0.0
-	while abs((guess**2) - num ) >= epsilon:
-		guess = guess + step
-	if abs((guess**2) - num) >= epsilon:
-		print(num)
-	else:
-		print(guess)
-	
+    
+    epsilon = 0.01
+    step = 0.1
+    num = int(input())
+    guess = 0.0
+    while abs((guess**2) - num ) >= epsilon:
+        guess = guess + step
+    if abs((guess**2) - num) >= epsilon:
+        print(num)
+    else:
+        print(guess)
+    
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
