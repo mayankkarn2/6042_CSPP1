467ab7c8e897555217dfc56e70bc276a1acdb2b0
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-03 15:45:10 +0530

    " -> 3 of 4 passed. pylint: 8.46/10 "

--- cspp1-assignments/m5/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -14,7 +14,7 @@ def main():
         perfect cube or not'''
     input_n = int(input())
     found_flag = 0
-    for x in range(1, INPUT_N+1, 1):
+    for x in range(1, input_n+1, 1):
         if x*x*x == input_n:
             found_flag = 1
             break
