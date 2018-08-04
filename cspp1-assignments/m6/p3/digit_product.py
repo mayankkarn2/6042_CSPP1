33d292c9fb6e9ff924364b6a28daca1d79ee19e2
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-04 10:26:33 +0530

    " -> 7 of 7 passed. pylint: -1.50/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -5,16 +5,26 @@ example:
 	output: 6
 '''
 def main():
-    '''
-    Read any number from the input, store it in variable int_input.
-    '''
-    int_input = int(input())
-    product = 1
-    temp_value = int_input 
-    while temp_value>0:
-    	rem_value = temp_value % 10
-    	product = product * rem_value
-    	temp_value = temp_value//10
-    print(product)
+	'''
+	Read any number from the input, store it in variable int_input.
+	'''
+	int_input = int(input())
+	product = 1
+	temp_value = int_input
+	if int_input == 0:
+		product = 0
+	elif int_input > 0:
+		while temp_value>0:
+			rem_value = temp_value % 10
+			product = product * rem_value
+			temp_value = temp_value//10
+	else:
+		temp_value = abs(temp_value)
+		while temp_value>0:
+			rem_value = temp_value % 10
+			product = product * rem_value
+			temp_value = temp_value//10
+		product = -1 * product
+	print(product)
 if __name__ == "__main__":
-    main()
+	main()
