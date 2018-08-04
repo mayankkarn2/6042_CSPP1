fab0be5eb7c3ad401384329650c5d7cfe3b6a3a0
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-04 11:02:54 +0530

    " -> 7 of 7 passed. pylint: 4.55/10 "

--- cspp1-assignments/m6/p2/special_char.py
+++ cspp1-assignments/m6/p2/special_char.py
@@ -8,12 +8,12 @@ def main():
     Read string from the input, store it in variable str_input.
     '''
     str_input = input()
-    LEN = len(str_input)
-    new = list(str_input)
-    for x in range(LEN):
-    	if new[x] == '!' or new[x] == '@' or new[x] == '#' or new[x] == '$' or new[x] == '%' or new[x] == '^' or new[x] == '&' or new[x] == '*':
-    		new[x] = ' '
-    str_input=''.join(new)
+    LEN_str = len(str_input)
+    n_s = list(str_input)
+    for x_i in range(LEN_str):
+    	if n_s[x_i] == '!' or n_s[x_i] == '@' or n_s[x_i] == '#' or n_s[x_i] == '$' or n_s[x_i] == '%' or n_s[x_i] == '^' or n_s[x_i] == '&' or n_s[x_i] == '*':
+    		n_s[x_i] = ' '
+    str_input=''.join(n_s)
     print(str_input)
 if __name__ == "__main__":
     main()
