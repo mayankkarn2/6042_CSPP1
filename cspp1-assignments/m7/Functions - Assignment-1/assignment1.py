9675e62e6b0665530fff1c6f17aef9601765c7a7
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-06 17:54:22 +0530

    " -> 10 of 10 passed. pylint: 9.29/10 "

--- cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
@@ -31,10 +31,11 @@ remaining balance at the end of the year in the format:
 
 def paying_debt_offinayear(balance_amount, annual_interest_rate, monthly_payment_rate):
     '''Input Balance, Annual interest, monthly payment'''
-    for in range(0, 12, 1):
+    for x_i in range(0, 12, 1):
         monthly_payment = monthly_payment_rate * balance_amount
         balance_amount = balance_amount - monthly_payment
         balance_amount = balance_amount + (annual_interest_rate/12) * balance_amount
+        x_i=x_i+1
     return round(balance_amount, 2)
 
 def main():
