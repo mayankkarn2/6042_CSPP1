68ea1ba4b8e5e02179100cd393ea7fdb50ee79e4
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-07 08:13:31 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
+++ cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
@@ -1,6 +1,6 @@
 '''# Assignment-2 - Paying Debt off in a Year
 
-# Now write a program that calculates the minimum fixed monthly payment 
+# Now write a program that calculates the minimum fixed monthly payment
 needed in order pay off a credit card balance within 12 months.
 # By a fixed monthly payment, we mean a single number which does not change
  each month, but instead is a constant amount that will be
@@ -16,12 +16,12 @@ needed in order pay off a credit card balance within 12 months.
  will pay off all debt in under 1 year, for example:
 # Lowest Payment: 180
 
-# Assume that the interest is compounded monthly according to the balance 
+# Assume that the interest is compounded monthly according to the balance
 at the end of the month (after the payment for that month is
 # made).
-# The monthly payment must be a multiple of $10 and is the same for 
+# The monthly payment must be a multiple of $10 and is the same for
 all months. Notice that it is possible for the balance to become
-# negative using this payment scheme, which is okay. A summary of the 
+# negative using this payment scheme, which is okay. A summary of the
 required math is found below:
 # Monthly interest rate = (Annual interest rate) / 12.0
 # Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
@@ -49,6 +49,5 @@ def main():
     data = data.split(' ')
     data = list(map(float, data))
     print(paying_debtoffinayear(data[0], data[1]))
-    
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
