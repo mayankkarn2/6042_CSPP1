8d6e4a74b42848782779e0020b8f7dec70bb147c
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-08 19:56:23 +0530

    " -> 9 of 9 passed. pylint: 9.17/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -8,40 +8,40 @@ secret_word are in letters_guessed) and False otherwise.
 
 
 def is_word_guessed(secret_word, letters_guessed):
-	'''
-	secret_word: string, the word the user is guessing
-	letters_guessed: list, what letters have been guessed so far
-	returns: boolean, True if all the letters of secret_word are in letters_guessed;
-	  False otherwise
-	'''
-	i = 0
-	found = 0
-	while i<len(secret_word):
-		if secret_word[i] in letters_guessed:
-			found += 1
-			i += 1
-		else:
-			i += 1
-	if found >= len(secret_word):
-		return True
-	else:
-		return False
+    '''
+    secret_word: string, the word the user is guessing
+    letters_guessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secret_word are in letters_guessed;
+      False otherwise
+    '''
+    i = 0
+    found = 0
+    while i < len(secret_word):
+        if secret_word[i] in letters_guessed:
+            found += 1
+            i += 1
+        else:
+            i += 1
+    if found >= len(secret_word):
+        return True
+    else:
+        return False
 
 def main():
-	'''
-	Main function for the program
-	'''
-	user_input = input()
-	if user_input:
-		data = user_input.split()
-		secret_word = data[0]
-	else:
-		data = []
-		secret_word = ""
-	list1 = []
-	for j in range(1, len(data)):
-		list1.append(data[j][0])
-	print(is_word_guessed(secret_word, list1))
+    '''
+    Main function for the program
+    '''
+    user_input = input()
+    if user_input:
+        data = user_input.split()
+        secret_word = data[0]
+    else:
+        data = []
+        secret_word = ""
+    list1 = []
+    for j in range(1, len(data)):
+        list1.append(data[j][0])
+    print(is_word_guessed(secret_word, list1))
 
 if __name__ == "__main__":
-	main()
+    main()
