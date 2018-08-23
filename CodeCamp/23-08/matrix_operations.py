76b5841dbe98472ec3efba30bce81ce0450ec617
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-23 12:02:56 +0530

    " -> 0 of 8 passed. pylint: 5.00/10 "

--- CodeCamp/23-08/matrix_operations.py
+++ CodeCamp/23-08/matrix_operations.py
@@ -1,29 +1,29 @@
-# def mult_matrix(m1, m2):
-#     '''
-#         check if the matrix1 columns = matrix2 rows
-#         mult the matrices and return the result matrix
-#         print an error message if the matrix shapes are not valid for mult
-#         and return None
-#         error message should be "Error: Matrix shapes invalid for mult"
-#     '''
-#     row1 = len(m1)
-#     col1 = len(m1[0])
-#     row2 = len(m2)
-#     col2 = len(m2[0])
-#     if col1 != row2:
-#         print("Error: Matrix shapes invalid for mult")
-#         return
-#     else:
-#         mul_matrix = []
-#         for i in range(0,row1,1):
-#             temp = []
-#             for j in range(0,col1,1):
-#                 add = 0
-#                 for k in range(0,col1,1):
-#                     add = add + m1[i][k] * m2[k][j]
-#                 temp.append(add)
-#             mul_matrix.append(temp)
-#         print(mul_matrix)
+def mult_matrix(m1, m2):
+    '''
+        check if the matrix1 columns = matrix2 rows
+        mult the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for mult
+        and return None
+        error message should be "Error: Matrix shapes invalid for mult"
+    '''
+    row1 = len(m1)
+    col1 = len(m1[0])
+    row2 = len(m2)
+    col2 = len(m2[0])
+    if col1 != row2:
+        print("Error: Matrix shapes invalid for mult")
+        return
+    else:
+        mul_matrix = []
+        for i in range(0,row1,1):
+            temp = []
+            for j in range(0,col1,1):
+                add = 0
+                for k in range(0,col1,1):
+                    add = add + m1[i][k] * m2[k][j]
+                temp.append(add)
+            mul_matrix.append(temp)
+        print(mul_matrix)
 
 def add_matrix(m1, m2):
     '''
@@ -79,7 +79,6 @@ def read_matrix():
         for j in rows:
             k.append(int(j))
         m2.append(k)
-    print(m2)
     add_matrix(m1, m2)
     # mult_matrix(m1, m2)
 
