01eb21cb96b112a1be5780c6e6f0df77c2db7c89
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-23 15:13:26 +0530

    " -> 1 of 8 passed. pylint: 5.49/10 "

--- CodeCamp/23-08/matrix_operations.py
+++ CodeCamp/23-08/matrix_operations.py
@@ -85,9 +85,9 @@ def read_matrix():
             for j in rows:
                 k.append(int(j))
             m2.append(k)
-        if len(m2) != int(dim2[0]) or len(m2[0]) != int(dim2[1]):
+        if len(m2) != int(dim2[0]) or [len(i) != int(dim2[1]) for i in m2]:
             print("Error: Invalid input for the matrix")
-        elif len(m1) != int(dim1[0]) or len(m1[0]) != int(dim1[1]):
+        elif len(m1) != int(dim1[0]) or [len(i) != int(dim1[1]) for i in m1]:
             print("Error: Invalid input for the matrix")
         else:
             print(add_matrix(m1, m2))
