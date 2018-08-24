9eeb8023523a474ded67e2695365be389ab01672
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-24 20:47:22 +0530

    " -> 14 of 14 passed. pylint: 9.41/10 "

--- CodeCamp/24-08/tic_tac_toe.py
+++ CodeCamp/24-08/tic_tac_toe.py
@@ -61,7 +61,7 @@ def is_valid_game(grid):
                 count_o += 1
             elif j == '.':
                 count_dot += 1
-    if abs(count_x - count_o) > 1:
+    if abs(count_x - count_o > 1):
         return False
     elif abs(count_x - count_o == 0) and count_dot > 0:
         return False
